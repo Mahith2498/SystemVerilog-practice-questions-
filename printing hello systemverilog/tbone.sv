53e6f2002e0beb78a1972366569371aed0d33247
MAHITH VATHSAV <vathsavkothapalli12092004@gmail.com>
2026-02-08 09:48:48 +0530

    Add files via upload

---
+++ printing hello systemverilog/tbone.sv
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 02/07/2026 12:59:14 PM
+// Designer Name: KOTHAPALLI MAHITH VATHSAV 
+// Module Name: tbone
+// Project Name: Hello SystemVerilog program  
+module tbone;
+string prog = "Hello SystemVerilog!!";
+initial begin 
+$display("%s",prog);
+end
+endmodule
