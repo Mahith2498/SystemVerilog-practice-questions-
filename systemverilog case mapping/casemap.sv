a8b7c4b0b30aa3fddaddea50779763a8b4092707
MAHITH VATHSAV <vathsavkothapalli12092004@gmail.com>
2026-03-03 21:05:36 +0530

    Add files via upload

---
+++ systemverilog case mapping/casemap.sv
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 02/14/2026 03:56:31 PM
+// Designer Name: KOTHAPALLI MAHITH VATHSAV 
+// Module Name: casemap
+// Project Name: 
+module casemap(
+
+    );
+    string a = "Error 404";
+    initial begin 
+    $display("The lowercase of Error 404 is :- %s", s.tolower());
+    end 
+endmodule
